677eb5888f563b70b87abffd5b7e670f35c03de0
Guilherme Jacichen <jacichen@alunos.utfpr.edu.br>
2021-03-18 20:49:22 -0300

    Initial commit

---
+++ src/main/kotlin/me/guijaci/ebanx/homeassignment/HomeAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package me.guijaci.ebanx.homeassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomeAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<HomeAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/me/guijaci/ebanx/homeassignment/HomeAssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.guijaci.ebanx.homeassignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HomeAssignmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
